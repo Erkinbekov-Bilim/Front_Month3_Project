da7fa5bb9fec4a13cdc38e92645c4ff32d313cb8
Erkinbekov-Bilim <erkinbekov298@gmail.com>
2024-12-15 00:21:53 +0600

    Homework three - modal and autoSlider

--- js/lesson.js
+++ js/lesson.js
@@ -15,3 +15,57 @@ phoneButton.onclick = () => {
         phoneResult.style.color = "red"
     }
 }
+
+// TAB SLIDER
+
+const tabContentBlocks = document.querySelectorAll('.tab_content_block')
+const tabs = document.querySelectorAll('.tab_content_item')
+const tabsParent = document.querySelector('.tab_content_items')
+let currentTab = 0
+
+const hideTabContent = () => {
+    tabContentBlocks.forEach((item) => {
+        item.style.display = 'none'
+    })
+
+    tabs.forEach((item) => {
+        item.classList.remove('tab_content_item_active')
+    })
+}
+
+const showTabContent = (index = 0) => {
+    tabContentBlocks[index].style.display = 'block'
+    tabs[index].classList.add('tab_content_item_active')
+}
+
+const autoSlider = () => {
+    currentTab++
+    if (currentTab > tabContentBlocks.length - 1) {
+        currentTab = 0
+    }
+    console.log(currentTab)
+    hideTabContent()
+    showTabContent(currentTab)
+
+}
+
+hideTabContent()
+showTabContent()
+setInterval(() => {
+    autoSlider()
+}, 3000)
+
+tabsParent.onclick = (event) => {
+    if (event.target.classList.contains('tab_content_item')) {
+        tabs.forEach((item, index) => {
+            if (event.target === item) {
+                hideTabContent()
+                currentTab = index
+                showTabContent(index)
+            }
+        })
+    }
+}
+
+
+
