0f9c251ec27d6c43897bc1a794ed33803f82f5a7
Erkinbekov-Bilim <erkinbekov298@gmail.com>
2024-12-21 19:14:27 +0600

    Homework six - card switcher and fetch request

--- js/lesson.js
+++ js/lesson.js
@@ -89,8 +89,8 @@ const converter = (element, targetElements) => {
                 console.log(`Target element value: ${targetElement.value} - ${targetElement.id}`)
 
                 if (element.id === "som") {
-                    if (targetElement.id === "usd") {
                         targetElement.value = (element.value / data.usd).toFixed(2)
+                        if (targetElement.id === "usd") {
                     }else if (targetElement.id === "eur") {
                         targetElement.value = (element.value / data.eur).toFixed(2)
                     }
@@ -122,3 +122,55 @@ const converter = (element, targetElements) => {
 converter(somInput, [usdInput, eurInput])
 converter(usdInput, [somInput, eurInput])
 converter(eurInput, [somInput, usdInput])
+
+// CARD SWITCHER
+
+const cardBlock = document.querySelector('.card')
+const btnNext = document.querySelector('#btn-next')
+const btnPrev = document.querySelector('#btn-prev')
+let id = 1
+
+
+const fetchDataTodo = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/todos/${id}`)
+        .then(response => response.json())
+        .then(data => {
+            const {title, completed, id} = data
+            cardBlock.innerHTML = `
+                <p>${title}</p>
+                <p>${completed}</p>
+                <p>${id}</p>
+            `
+        })
+        .catch((error) => console.log(error.message))
+}
+
+const updateIdForTodo = (btnType) => {
+    if (btnType === 'next') {
+        id = id === 200 ? 1 : id + 1;
+    } else if (btnType === 'prev') {
+        id = id === 1 ? 200 : id - 1;
+    }
+    fetchDataTodo(id)
+}
+
+const nextAndPrev = () => {
+    btnNext.onclick = () => updateIdForTodo('next')
+    btnPrev.onclick = () => updateIdForTodo('prev')
+}
+
+fetchDataTodo(id)
+nextAndPrev()
+
+// Fetch API
+
+const fetchDataPosts = () => {
+    console.log("Fetch запрос на https://jsonplaceholder.typicode.com/posts")
+    fetch("https://jsonplaceholder.typicode.com/posts")
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((error) => console.log(error.message))
+}
+
+
+fetchDataPosts()
